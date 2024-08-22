28a67a6e2ecee6f34e28095f5a25f25993a167f6
JehooJeon <jjh0211sg@gmail.com>
2024-08-22 22:20:33 +0900

    [Bronze V] Title: 윤년, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,12 +1,10 @@
 year = int(input())
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print(1)
-        else:
-            print(0)
-    else:
-        print(1)
+if year % 400 == 0:
+    print(1)
+elif year % 100 == 0:
+    print(0)
+elif year % 4 == 0:
+    print(1)
 else:
     print(0)
