d5943e7ee470bd88eba6c74ab7daf2391ce95d21
JehooJeon <jjh0211sg@gmail.com>
2024-10-21 10:49:03 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -6,7 +6,7 @@ FROM
     item_info ii
 LEFT JOIN
     item_tree it ON ii.item_id = it.item_id
-WHERE
-    it.parent_item_id IS NULL
+WHERE 1 = 1
+    AND it.parent_item_id IS NULL
 ORDER BY
-    ii.item_id;
+    ii.item_id ASC;
