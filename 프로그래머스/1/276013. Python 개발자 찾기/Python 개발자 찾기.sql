fac1c48fb105780187c79703c5b87718cc6f04c7
JehooJeon <jjh0211sg@gmail.com>
2024-10-21 10:23:37 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -4,8 +4,11 @@ SELECT
     email,
     first_name,
     last_name
-FROM developer_infos
-WHERE SKILL_1 = 'Python'
-OR SKILL_2 = 'Python'
-OR SKILL_3 = 'Python'
-ORDER BY 1;
+FROM 
+    developer_infos
+WHERE 1 = 1
+    AND SKILL_1 = 'Python'
+    OR SKILL_2 = 'Python'
+    OR SKILL_3 = 'Python'
+ORDER BY 
+    id;
