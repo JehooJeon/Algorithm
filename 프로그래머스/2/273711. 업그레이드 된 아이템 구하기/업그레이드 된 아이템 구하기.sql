d3646254ed89abc57a42df109a68b437e39ac400
JehooJeon <jjh0211sg@gmail.com>
2024-10-21 10:56:33 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -9,8 +9,11 @@ JOIN
     item_tree it ON ii.item_id = it.item_id
 WHERE 1 = 1
     AND it.parent_item_id IS NOT NULL
-    AND parent_item_id IN (SELECT item_id
-                           FROM item_info
-                           WHERE rarity = 'RARE')
+    AND parent_item_id IN (SELECT 
+                               item_id
+                           FROM 
+                               item_info
+                           WHERE 
+                               rarity = 'RARE')
 ORDER BY
     ii.item_id DESC;
