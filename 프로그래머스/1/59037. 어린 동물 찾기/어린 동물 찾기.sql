c7af41172f3bd52beaf2f30f1dfce3a23bb68843
JehooJeon <jjh0211sg@gmail.com>
2025-03-07 15:41:39 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,10 +1,9 @@
--- 코드를 입력하세요
-SELECT 
-    animal_id, 
+SELECT
+    animal_id,
     name
-FROM 
+FROM
     animal_ins
-WHERE 1 = 1
-    AND intake_condition != "Aged"
-ORDER BY 
-    animal_id ASC;
+WHERE
+    intake_condition NOT LIKE 'Aged'
+ORDER BY
+    animal_id ASC
