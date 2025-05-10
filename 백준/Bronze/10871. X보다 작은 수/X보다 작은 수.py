2d292fc1ddf92f822028357766cdf149e2d42787
JehooJeon <jjh0211sg@gmail.com>
2025-05-10 13:57:36 +0900

    [Bronze V] Title: X보다 작은 수, Time: 108 ms, Memory: 111036 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,6 +1,17 @@
+# N, A : 정수 N개로 이루어진 수열 A
+# X : 정수
+# A에서 X보다 작은 수를 모두 출력
+def solution(N, X, A):
+    answer = []
+
+    for a in A:
+        if a < X:
+            answer.append(a)
+    
+    return answer        
+
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
-
-for i in A:
-    if i < X:
-        print(i, end=' ')
+answer = solution(N, X, A)
+for i in answer:
+    print(i, end=' ')
