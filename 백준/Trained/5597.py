5c7af81fe157143dd6e8da957f6c35eba7fc5a89
JehooJeon <jjh0211sg@gmail.com>
2025-05-10 17:05:46 +0900

    [BOJ-5597] 과제 안 내신 분..?
    * min, max 조건 추가

--- 백준/Trained/5597.py
+++ 백준/Trained/5597.py
@@ -2,15 +2,16 @@ import sys
 
 def find_missing_number():
     arr = list(range(1, 31))
+    
     for _ in range(28):
         n = int(sys.stdin.readline())
         arr.remove(n)
-    for a in arr:
-        print(a)
+    
+    print(min(arr))
+    print(max(arr))
 
 def main():
     find_missing_number()
 
 if __name__ == "__main__":
     main()
-    
