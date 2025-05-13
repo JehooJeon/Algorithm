027de536c8b90af1c99e61c2a982fd5c336f5940
JehooJeon <jjh0211sg@gmail.com>
2025-05-13 15:55:07 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 720 ms, Memory: 117028 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,17 +1,15 @@
+from collections import Counter
+
 def main():
     N = int(input())
     A = list(map(int, input().split()))
     M = int(input())
     B = list(map(int, input().split()))
-
-    cards = {int(key): 0 for key in B}
-
-    for a in A:
-        if a in cards:
-            cards[a] += 1
     
-    for key in B:
-        print(cards[key], end=' ')
+    counter = Counter(A)
+    
+    for b in B:
+        print(counter[b], end=' ')
 
 if __name__ == "__main__":
     main()
