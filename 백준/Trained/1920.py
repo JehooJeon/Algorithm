1ecb98c66fdff0215975353b9466f0d4ded80378
대전_1반_B106_전제후 <jjh0211sg@naver.com>
2025-05-13 10:44:20 +0900

    [BOJ-1920] 수 찾기

---
+++ 백준/Trained/1920.py
@@ -0,0 +1,14 @@
+def main():
+    N = int(input())
+    A = set(map(int, input().split()))
+    M = int(input())
+    B = list(map(int, input().split()))
+
+    for i in range(M):
+        if B[i] in A:
+            print(1)
+        else:
+            print(0)
+
+if __name__ == "__main__":
+    main()
