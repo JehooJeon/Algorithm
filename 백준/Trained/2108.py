7477917655cdcd685fc9791db90eb74d1294033d
대전_1반_B106_전제후 <jjh0211sg@naver.com>
2025-05-14 12:42:35 +0900

    [BOJ-2108] 통계학

---
+++ 백준/Trained/2108.py
@@ -0,0 +1,33 @@
+import sys
+from collections import Counter
+
+def mean(arr):
+    return round(sum(arr) / len(arr))
+
+def median(arr):
+    return arr[len(arr) // 2]
+
+def mode(arr):
+    dic = Counter(arr)
+    max_value = max(dic.values())
+    modes = [k for k, v in dic.items() if v == max_value]
+    
+    if len(modes) == 1:
+        return modes[0]
+    return modes[1]
+
+def scale(arr):
+    return max(arr) - min(arr)
+
+def main():
+    N = int(input())
+    arr = list(int(sys.stdin.readline()) for _ in range(N))
+    arr.sort()
+
+    print(mean(arr))
+    print(median(arr))
+    print(mode(arr))
+    print(scale(arr))
+
+if __name__ == "__main__":
+    main()
