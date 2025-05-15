2027b8265a4710e10641d52d190b577ebc701aa4
JehooJeon <jjh0211sg@gmail.com>
2025-05-15 15:36:06 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 7716 ms, Memory: 52044 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,19 @@
+def main():
+    N, M = map(int, input().split())
+    poketmon_name = {}
+    poketmon_num = {}
+
+    for i in range(1, N + 1):
+        name = input()
+        poketmon_name[name] = i
+        poketmon_num[i] = name
+
+    for _ in range(M):
+        problem = input()
+        if problem.isdigit():
+            print(poketmon_num[int(problem)])
+        else:
+            print(poketmon_name[problem])
+
+if __name__ == "__main__":
+    main()
